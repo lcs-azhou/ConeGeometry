4433b31060e7da89d19852c30ceb5b05ab4341b5
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-16 21:19:45 -0500

    Initial Commit

---
+++ ConeGeometry/ConeGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConeGeometryApp.swift
+//  ConeGeometry
+//
+//  Created by Ansheng Zhou on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct ConeGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
